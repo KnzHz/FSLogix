204f17c9fe78a92584acabdc4f36895bf7f21a63
KnzHz <36756890+KnzHz@users.noreply.github.com>
2020-12-16 13:09:28 +0100

    Update FSLogix Profile Size Warning.ps1
    Added extra layers of warning and have it loop for hard warnings to check looped through the session.

--- FSLogix Profile Size Warning.ps1
+++ FSLogix Profile Size Warning.ps1
@@ -1,5 +1,6 @@
 # ****************************************************
 # D. Mohrmann, S&L Firmengruppe, Twitter: @mohrpheus78
+# Super charged by KnzHz
 # ****************************************************
 
 <#
@@ -8,6 +9,10 @@ Shows a message to user in the notificarion area if FSLogix profile is almost fu
 		
 .DESCRIPTION
 Gets information about the users FSLogix profile (size and remaining size) and calculates the free space in percent.
+
+Layers of Warnings:
+There two percentages to think of here, Hard and Soft warning, each has a different message and HARD warnings have an initial Popup box 
+and each loop will then be notifications after that. Soft Warning will only be ONCE when the script starts, after initial wait time. 
 		
 .EXAMPLE
 .FSLogix Profile Size Warning.ps1
@@ -15,11 +20,35 @@ Gets information about the users FSLogix profile (size and remaining size) and c
 .NOTES
 This script must be run on a machine where the user is currently logged on.
 Should be run as a powershell login script via GPO.
-Edit value $PercentFree -le 10 in line 34 to define the free percent.
 #>
 
+#######################################
+#Configurable Section START
+
+$firstwarn = 1
+#Initial wait time
+$Waittimefirst= 1
+#Time in seconds between each check
+$looptime = 3600
+#Percentage of free space on Soft warning
+$PercentFreeWarningSoft = 10
+#Percentage of free space on Hard warning
+$PercentFreeWarningHard = 5
+
+#Configurable Section END
+#######################################
+#Just for the while loop
+$run = 1
+#Define first loop
+$firstrun = 1
+#Define first Hard warning
+
+
+
+
+Start-Sleep $Waittimefirst
+while($run -eq 1) {
 # Wait 10 sec. till showing the message
-Start-Sleep 10
 
 # Get the relevant informations from the FSLogix profile
 $FSLProfileSize = Get-Volume -FileSystemLabel *Profile-$ENV:USERNAME* | Where-Object { $_.DriveType -eq 'Fixed'}
@@ -29,7 +58,27 @@ IF (!($FSLProfileSize -eq $nul))
 {
 	# Calculate the free space in percent
 	$PercentFree = [Math]::round((($FSLProfileSize.SizeRemaining/$FSLProfileSize.size) * 100))
+    $whatsleft = [Math]::round((($FSLProfileSize.SizeRemaining/1024)/1024/1024),1)
 
-	# If free space is less then 10 % show message
-	IF ($PercentFree -le 10) {wlrmdr -s 25 -f 2 -t FSLogix Profile -m Warning! Your profile contingent is almost exhausted. Please inform the IT service!}
+	# If free space is less then Hard limit show message
+	IF ($PercentFree -le $PercentFreeWarningHard) 
+    {
+        if($firstwarn -eq 1) 
+        {
+          [System.Windows.MessageBox]::Show('You have almost no space left in your profile: ' + $whatsleft + ' GiB.' + "`n`n" + 'Clean out your profile or contact support!','Warning: Almost no Profile diskspace!','OK','Error') 
+          $firstwarn = 0 
+        } else {
+            wlrmdr -s 25 -f 3 -t Warning: Almost no Profile diskspace!  -m You have almost no space left in your profile: $whatsleft GiB. Clean out your profile or contact support!
+        }
+    }
+
+    # First time we just warn that people are close 
+    IF ($PercentFree -le $PercentFreeWarningSoft -and $firstrun -eq 1) 
+    {
+        wlrmdr -s 25 -f 3 -t Warning: Low on Profile diskspace!  -m You are using most of your diskpace in your profile: $whatsleft GiB. Clean out your profile or contact support!
+        $firstrun = 0
+    }
+
+}
+Start-Sleep $looptime
 }
